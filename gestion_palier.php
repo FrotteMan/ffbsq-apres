7f54077522fa4d549eaa0bec7dd0f60ae7a02d6b
Julien <julienl.science@gmail.com>
2025-04-25 14:06:33 +0200

    Mise à jour

--- gestion_palier.php
+++ gestion_palier.php
@@ -12,10 +12,12 @@ include 'menu.php';
     <h2 class="text-center">Gestion des Paliers</h2>
 
     <?php
-    $pdo = new PDO('mysql:host=localhost;dbname=ffbsq_competitions', 'root', 'root');
+    //$pdo = new PDO('mysql:host=localhost;dbname=ffbsq_competitions', 'root', 'root');
 
-    $sql = $pdo->query("SELECT * FROM palier ORDER BY nbr_pts_minimum_palier ASC");
-    $paliers = $sql->fetchAll();
+    //$sql = $pdo->query("SELECT * FROM palier ORDER BY nbr_pts_minimum_palier ASC");
+    //$paliers = $sql->fetchAll();
+	$json = file_get_contents('http://localhost/mon-api/api-palier.php')
+	$paliers = json_decode($json, true);
     ?>
 
     <table class="table table-hover">
